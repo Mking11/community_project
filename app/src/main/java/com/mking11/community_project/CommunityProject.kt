95b8d5bd9aa54f82130758a800ec8707f8a37922
michael <michaelboymek@gmail.com>
2021-12-05 13:06:40 +0300

    initial  :Community project Michael Mekonnen

---
+++ app/src/main/java/com/mking11/community_project/CommunityProject.kt
@@ -0,0 +1,10 @@
+package com.mking11.community_project
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CommunityProject:Application(){
+
+
+}
---
+++ app/src/main/java/com/mking11/community_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mking11.community_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
